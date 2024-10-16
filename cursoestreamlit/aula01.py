4401a3ec3e1ecbd01e595b2c4e2a731320616c45
otilevad <arthurdavelvb@gmail.com>
2024-10-16 11:49:33 -0300

    aula05

--- cursoestreamlit/aula01.py
+++ cursoestreamlit/aula01.py
@@ -9,27 +9,29 @@ import pandas as std
 import json
 
 st.title("Dashboard de Vendas:shopping_trolley: ")
-url="https://labdados.com/produtos"
+url="https://labdados.com/produtos" # https://labdados.com/produtos?regiao=norte&ano=2022
 response= requests.get(url)
 
+# json > convertido para dicionário > Convertido para databframe
 df = pd.DataFrame.from_dict(response.json())
 if st.button("todos"):
     st.balloons()
         
-    def formata_valor(valor):
-        if valor >= 1000000:
+    def muda_valor(valor):
+        if valor == 1000000:
+            return f" {valor/1000000:.1f} milhão"
+        elif valor > 1000000:
             return f" {valor/1000000:.1f} milhões"
         elif valor >= 1000:
             return f" {valor/1000:.1f} mil"
         else:
             return f" {valor:.2f}"      
         
-    st.metric("Receita:",formata_valor(df['Preço'].sum()))
-    st.metric("Quantidade de vendas (linhas)",formata_valor(df.shape[0]))
+    st.metric("Receita:",muda_valor(df['Preço'].sum()))
+    st.metric("Quantidade de vendas (linhas)",muda_valor(df.shape[0]))
     st.metric("Quantidade de variaveis (colunas)",df.shape[1])
     
     st.dataframe(df)
     st.snow()
 else:
     st.write("Clique no botão todos")
-    
